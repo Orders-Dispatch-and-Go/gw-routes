15bf5990ce32062de628c7f09383b3c953081ff9
meliosu <alex.atyurin@gmail.com>
2025-12-19 20:59:52 +0700

    trips

--- src/api/service/router.rs
+++ src/api/service/router.rs
@@ -13,7 +13,7 @@ pub fn router(state: super::State) -> axum::Router {
             get(get_cargo_request_points),
         )
         .route("/routes/trips/{id}/points", get(get_trip_points))
-        .route("/routes/tips/potential", post(get_potential_routes))
+        .route("/routes/trips/potential", post(get_potential_routes))
         .route("/routes/trips/merge", post(merge_routes))
         .route("/routes/trips/cargo_request/remove", post(remove_stations))
         .route("/stations/{id}", get(get_station))
